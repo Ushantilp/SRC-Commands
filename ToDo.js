595bfadae7ec857efd4ca9714782077ad93f8d0a
Ushanti <ushanti.lp@gmail.com>
2024-02-12 19:07:44 +0100

    Fixed Files don't get read properly

--- ToDo.js
+++ ToDo.js
@@ -1,24 +1,24 @@
 /*
-++TODO++ 
+##TODO 
 TODO 9:	PB Command schreiben 
 TODO 8:	PB um Individual Level erweitern
 TODO 7:	Merging IlHandler.js und LvLHandler.js
-TODO 6:	ID & Kürzel & Name Nutzbar machen
-TODO 5:	Namen ausgeben lassen für Game, Category und Variablen (getname Command)
-TODO 4:	Weitere Variablen erst ermöglichen wenn vorherige gesetzt wurden 
-	 	(if var ID1 undefined = nutze erst var ID1 usw.)
-TODO 3:	Wenn datenbank eintrag "Null" oder "undefined" entsprechende Fehlermeldung ausgeben
-TODO 2:	Code aufräumen [WIP]
-TODO 1:	Credits hinzufügen
+TODO 6:	ID & KÃ¼rzel & Name Nutzbar machen
+TODO 5:	Namen ausgeben lassen fÃ¼r Game, Category und Variablen (getname Command)
+TODO 4:	Weitere Variablen erst ermÃ¶glichen wenn vorherige gesetzt wurden
+		(if var ID1 undefined = nutze erst var ID1 usw.)
+TODO 3.	Wenn datenbank eintrag "Null" oder "undefined" entsprechende Fehlermeldung ausgeben
+TODO 2:	Code aufrÃ¤umen [WIP]
+TODO 1:	Credits hinzufÃ¼gen
 
-++DONE++
-DONE 1:	ID Commands überarbeiten (set, del, get)
-DONE 2:	Variablen Löschbar machen?
+##DONE
+DONE 1:	ID Commands Ã¼berarbeiten (set, del, get)
+DONE 2:	Variablen LÃ¶schbar machen?
 DONE 3:	Modes aus den Commands entfernen
 DONE 4:	Variablen Call Fixen
 		(variablen wurden im call nicht mit einbezogen)
-DONE 5:	Workaround für den PB Command geschrieben
-DONE 6:	Mode Handling überarbeiten
+DONE 5: Workaround fÃ¼r den PB Command geschrieben
+DONE 6:	Mode Handling Ã¼berarbeiten
 		!addmode [erstellt einen neuen Mode]?,
 		!setmode [legt einen Mode fest]?,
 		!listmode [listet alle Modes auf],
