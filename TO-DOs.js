dfef7260003e921e8ed49cfeae2aff9dd0926c6e
Ushanti <ushanti.lp@gmail.com>
2024-02-12 18:13:05 +0100

    File added for more overview

---
+++ TO-DOs.js
@@ -0,0 +1,31 @@
+/*
+++TODO++ 
+TODO 9:	PB Command schreiben 
+TODO 8:	PB um Individual Level erweitern
+TODO 7:	Merging IlHandler.js und LvLHandler.js
+TODO 6:	ID & Kürzel & Name Nutzbar machen
+TODO 5:	Namen ausgeben lassen für Game, Category und Variablen (getname Command)
+TODO 4:	Weitere Variablen erst ermöglichen wenn vorherige gesetzt wurden 
+	 	(if var ID1 undefined = nutze erst var ID1 usw.)
+TODO 3:	Wenn datenbank eintrag "Null" oder "undefined" entsprechende Fehlermeldung ausgeben
+TODO 2:	Code aufräumen [WIP]
+TODO 1:	Credits hinzufügen
+
+++DONE++
+DONE 1:	ID Commands überarbeiten (set, del, get)
+DONE 2:	Variablen Löschbar machen?
+DONE 3:	Modes aus den Commands entfernen
+DONE 4:	Variablen Call Fixen
+		(variablen wurden im call nicht mit einbezogen)
+DONE 5:	Workaround für den PB Command geschrieben
+DONE 6:	Mode Handling überarbeiten
+		!addmode [erstellt einen neuen Mode]?,
+		!setmode [legt einen Mode fest]?,
+		!listmode [listet alle Modes auf],
+		!getmode [gibt den aktuellen Mode aus]
+DONE 7:	WR Command um Individual Level erweitern 
+DONE 8:	WRHandler an Mehrspieler Spiele anpassen
+		(anzeige von mehr als einem Spieler)
+DONE 9:	WRHandler.js Error ausgabe wenn keine Runs gefunden werden
+		(Weil zB mit den Entsprechenden Variablen keine Runs gibt )
+*/
