729972b7bb746f849c5aa867f05840141f90dcd5
Ushanti <ushanti.lp@gmail.com>
2024-02-13 03:12:31 +0100

    ToDo's angepasst

--- ToDo.js
+++ ToDo.js
@@ -1,31 +1,32 @@
 /*
 ##TODO 
-TODO 9:	PB Command schreiben 
-TODO 8:	PB um Individual Level erweitern
-TODO 7:	Merging IlHandler.js und LvLHandler.js
-TODO 6:	ID & Kürzel & Name Nutzbar machen
-TODO 5:	Namen ausgeben lassen für Game, Category und Variablen (getname Command)
-TODO 4:	Weitere Variablen erst ermöglichen wenn vorherige gesetzt wurden
-		(if var ID1 undefined = nutze erst var ID1 usw.)
-TODO 3.	Wenn datenbank eintrag "Null" oder "undefined" entsprechende Fehlermeldung ausgeben
-TODO 2:	Code aufräumen [WIP]
-TODO 1:	Credits hinzufügen
+TODO 10: PB Command schreiben 
+TODO 09: PB um Individual Level erweitern
+TODO 08: Caller der die IDs aus der URL abruft schreiben
+TODO 07: Merging IlHandler.js und LvLHandler.js
+TODO 06: ID & Kürzel & Name Nutzbar machen
+TODO 05: Namen ausgeben lassen für Game, Category und Variablen (getname Command)
+TODO 04: Weitere Variablen erst ermöglichen wenn vorherige gesetzt wurden
+			(if var ID1 undefined = nutze erst var ID1 usw.)
+TODO 03. Wenn datenbank eintrag "Null" oder "undefined" entsprechende Fehlermeldung ausgeben
+TODO 02: Code aufräumen [WIP]
+TODO 01: Credits hinzufügen
 
 ##DONE
-DONE 1:	ID Commands überarbeiten (set, del, get)
-DONE 2:	Variablen Löschbar machen?
-DONE 3:	Modes aus den Commands entfernen
-DONE 4:	Variablen Call Fixen
+DONE 01: ID Commands überarbeiten (set, del, get)
+DONE 02: Variablen Löschbar machen?
+DONE 03: Modes aus den Commands entfernen
+DONE 04: Variablen Call Fixen
 		(variablen wurden im call nicht mit einbezogen)
-DONE 5: Workaround für den PB Command geschrieben
-DONE 6:	Mode Handling überarbeiten
-		!addmode [erstellt einen neuen Mode]?,
-		!setmode [legt einen Mode fest]?,
-		!listmode [listet alle Modes auf],
-		!getmode [gibt den aktuellen Mode aus]
-DONE 7:	WR Command um Individual Level erweitern 
-DONE 8:	WRHandler an Mehrspieler Spiele anpassen
-		(anzeige von mehr als einem Spieler)
-DONE 9:	WRHandler.js Error ausgabe wenn keine Runs gefunden werden
-		(Weil zB mit den Entsprechenden Variablen keine Runs gibt )
+DONE 05: Workaround für den PB Command geschrieben
+DONE 06: Mode Handling überarbeiten
+			!addmode [erstellt einen neuen Mode]?,
+			!setmode [legt einen Mode fest]?,
+			!listmode [listet alle Modes auf],
+			!getmode [gibt den aktuellen Mode aus]
+DONE 07: WR Command um Individual Level erweitern 
+DONE 08: WRHandler an Mehrspieler Spiele anpassen
+			(anzeige von mehr als einem Spieler)
+DONE 09: WRHandler.js Error ausgabe wenn keine Runs gefunden werden
+			(Weil zB mit den Entsprechenden Variablen keine Runs gibt )
 */
