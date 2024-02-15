2cbac78b1e21e5e6e70ab7f6fd5403b37a7f02ed
Ushanti <ushanti.lp@gmail.com>
2024-02-15 07:00:03 +0100

    ID Caller Command added

--- SRCCommands.js
+++ SRCCommands.js
@@ -54,7 +54,7 @@
 		$.registerChatCommand('./custom/SRC-Commands/Handler/GoalHandler.js', 'goal', 7);
 		$.registerChatCommand('./custom/SRC-Commands/Handler/LastRunHandler.js', 'lr', 7);
 		$.registerChatCommand('./custom/SRC-Commands/Handler/LBHandler.js', 'lb', 7);
-		//Caller Test
+		//ID Caller
 		$.registerChatCommand('./custom/SRC-Commands/SRCIDCaller.js', 'callid', 2);
 	
 	});
