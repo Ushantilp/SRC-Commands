2574951220b25b91e12b9d20af763c0107d3b011
Ushanti <ushanti.lp@gmail.com>
2024-02-16 21:11:07 +0100

    Updating the ToDo's

--- ToDo.js
+++ ToDo.js
@@ -1,15 +1,15 @@
 /*
 ##TODOs & Ideas
-TODO 09: Leaderboard Call in LBHandler.js fertig schreiben
-TODO 08: LBHandler.js um Individual Level erweitern
-TODO 07: Merging IlHandler.js und LvLHandler.js
-TODO 06: ID & Kürzel & Name Nutzbar machen
-TODO 05: Namen ausgeben lassen für Game, Category und Variablen (getname Command)
-TODO 04: Weitere Variablen erst ermöglichen wenn vorherige gesetzt wurden
+TODO 09: Check Goal und LastRun funktion
+TODO 08: Merging IlHandler.js und LvLHandler.js (Maybe?)
+TODO 07: ID & Kürzel & Name Nutzbar machen
+TODO 06: Namen ausgeben lassen für Game, Category und Variablen (getname Command)
+TODO 05: Weitere Variablen erst ermöglichen wenn vorherige gesetzt wurden
 			(if var ID1 undefined = nutze erst var ID1 usw.)
-TODO 03. Wenn datenbank eintrag "Null" oder "undefined" entsprechende Fehlermeldung ausgeben
-TODO 02: Code aufräumen [WIP]
-TODO 01: Credits hinzufügen
+TODO 04. Wenn datenbank eintrag "Null" oder "undefined" entsprechende Fehlermeldung ausgeben
+TODO 03: Code aufräumen [WIP]
+TODO 02: Credits hinzufügen
+TODO 01: ID Import aus der URL ermöglichen 
 
 ##DONE
 DONE 01: ID Commands überarbeiten (set, del, get)
@@ -31,5 +31,7 @@ DONE 09: WRHandler.js Error ausgabe wenn keine Runs gefunden werden
 DONE 10: PB Command schreiben 
 DONE 11: Caller der die IDs aus der URL abruft schreiben
 DONE 12: PB um Individual Level erweitern
-TODO 13: Im PBHandler Error Handling wenn kein PB verfügbar
+DONE 13: Im PBHandler Error Handling wenn kein PB verfügbar
+DONE 14: Leaderboard Call in LBHandler.js fertig schreiben
+DONE 15: LBHandler.js um Individual Level erweitern
 */
