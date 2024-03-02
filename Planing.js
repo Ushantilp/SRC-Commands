cff672a97a6399901d66fa2fb5bb13304c68c425
Ushanti <ushanti.lp@gmail.com>
2024-03-02 05:20:46 +0100

    Adding a Reminder for me temporary

---
+++ Planing.js
@@ -0,0 +1,27 @@
+/*
+DelHandler:
+!delmode = Löscht alles so wie den Mode & den Aktuellen Mode
+!resetmode = Löscht alle Einträge im Aktuellen Mode
+!delplayer = Löscht nur die PlayerID aus der Datenbank
+!delLR = löscht nur den Last Run Eintrag aus der Datenbank
+!delgoal = Löscht nur den Goal Eintrag aus der Datenbank
+
+SRCIDCaller:
+Caller umbenennen (Noch kein Name)
+Caller Command umbenennen (noch kein Name)
+Caller ausgabe anpassen (IDs entfernen und gesammt ausgabe des Hinzugefügten ausgeben)
+
+Datei Strucktur ändern
+SCR-Commands
+ -SRCCommands.js
+    -Handler
+     -ModeHandler.js
+     -WRHandler.js
+     -PBHandler.js
+     -LBHandler.js
+     -LRHandler.js
+     -GoalHandler.js
+
+
+*/
+
