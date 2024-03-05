e93b214ff9ba5794c52bc00b8ad4c8e53c275adb
Ushanti <ushanti.lp@gmail.com>
2024-03-05 04:38:05 +0100

    Updating the Commands #rework

--- SRCCommands.js
+++ SRCCommands.js
@@ -29,48 +29,27 @@
 	
 // Register commands and set premission levels (0 = broadcaster, 1 = Editor, 2 = moderator, 7 = viewer)
 	$.bind('initReady', function () {
+		//ID Caller
+		$.registerChatCommand('./custom/SRC-Commands/SRCIDCaller.js', 'src', 2);
 		//Mode Handling
 		$.registerChatCommand('./custom/SRC-Commands/Handler/ModeHandler.js', 'mode', 2);
 		$.registerChatCommand('./custom/SRC-Commands/Handler/ModeHandler.js', 'getmode', 2);
 		$.registerChatCommand('./custom/SRC-Commands/Handler/ModeHandler.js', 'listmode', 2);
-		//Player ID Handling
-		$.registerChatCommand('./custom/SRC-Commands/Handler/PlayerHandler.js', 'setplayerid', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/PlayerHandler.js', 'delplayerid', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/PlayerHandler.js', 'getplayerid', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/PlayerHandler.js', 'getplayername', 2);
-		//Game ID Handling
-		$.registerChatCommand('./custom/SRC-Commands/Handler/GameHandler.js', 'setgameid', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/GameHandler.js', 'delgameid', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/GameHandler.js', 'getgameid', 2);
-		//Category ID Handling
-		$.registerChatCommand('./custom/SRC-Commands/Handler/CatHandler.js', 'setcatid', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/CatHandler.js', 'delcatid', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/CatHandler.js', 'getcatid', 2);
-		//Level ID Handling
-		$.registerChatCommand('./custom/SRC-Commands/Handler/LvLHandler.js', 'setlvl', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/LvLHandler.js', 'dellvl', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/LvLHandler.js', 'getlvl', 2);
-		// IL Handling
-		$.registerChatCommand('./custom/SRC-Commands/Handler/IlHandler.js', 'setil', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/IlHandler.js', 'delil', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/IlHandler.js', 'getil', 2);
-		//Variables ID Handling
-		$.registerChatCommand('./custom/SRC-Commands/Handler/VarHandler.js', 'setvar', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/VarHandler.js', 'delvar', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/VarHandler.js', 'getvar', 2);
-		//Other Data Handling
-		$.registerChatCommand('./custom/SRC-Commands/Handler/GoalHandler.js', 'setgoal', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/GoalHandler.js', 'delgoal', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/LastRunHandler.js', 'setlr', 2);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/LastRunHandler.js', 'dellr', 2);
+		//Info Handling
+		$.registerChatCommand('./custom/SRC-Commands/Handler/InfoHandler.js', 'setlr', 2);
+		$.registerChatCommand('./custom/SRC-Commands/Handler/InfoHandler.js', 'setgoal', 2);
+		//Del Handling
+		$.registerChatCommand('./custom/SRC-Commands/Handler/DelHandler.js', 'resetmode', 2);
+		$.registerChatCommand('./custom/SRC-Commands/Handler/DelHandler.js', 'delmode', 2);
+		$.registerChatCommand('./custom/SRC-Commands/Handler/DelHandler.js', 'delplayer', 2);
+		$.registerChatCommand('./custom/SRC-Commands/Handler/DelHandler.js', 'dellr', 2);
+		$.registerChatCommand('./custom/SRC-Commands/Handler/DelHandler.js', 'delgoal', 2);
 		//Read Data Commands
 		$.registerChatCommand('./custom/SRC-Commands/Handler/WRHandler.js', 'wr', 7);
 		$.registerChatCommand('./custom/SRC-Commands/Handler/PBHandler.js', 'pb', 7);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/GoalHandler.js', 'goal', 7);
-		$.registerChatCommand('./custom/SRC-Commands/Handler/LastRunHandler.js', 'lr', 7);
+		$.registerChatCommand('./custom/SRC-Commands/Handler/InfoHandler.js', 'goal', 7);
+		$.registerChatCommand('./custom/SRC-Commands/Handler/InfoHandler.js', 'lr', 7);
 		$.registerChatCommand('./custom/SRC-Commands/Handler/LBHandler.js', 'lb', 7);
-		//ID Caller
-		$.registerChatCommand('./custom/SRC-Commands/SRCIDCaller.js', 'callid', 2);
 	
 	});
 })();
